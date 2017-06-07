41e277f52d1d8d244ea7587be67408fac231ad31
Joel Niklaus <me@joelniklaus.ch>
2017-06-07 08:35:21 +0200

    some work on unittests

--- app/src/test/java/ch/joelniklaus/indoloc/unitTests/WekaHelperUnitTest.java
+++ app/src/test/java/ch/joelniklaus/indoloc/unitTests/WekaHelperUnitTest.java
@@ -1,15 +1,28 @@
 package ch.joelniklaus.indoloc.unitTests;
 
+import android.support.annotation.NonNull;
+
 import org.apache.commons.lang3.SerializationUtils;
 import org.junit.Test;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 import ch.joelniklaus.indoloc.AbstractTest;
+import ch.joelniklaus.indoloc.exceptions.CouldNotLoadArffException;
 import ch.joelniklaus.indoloc.exceptions.DifferentHeaderException;
+import ch.joelniklaus.indoloc.exceptions.InvalidRoomException;
 import ch.joelniklaus.indoloc.helpers.FileHelper;
 import ch.joelniklaus.indoloc.helpers.WekaHelper;
+import ch.joelniklaus.indoloc.models.DataPoint;
+import ch.joelniklaus.indoloc.models.LocationData;
+import ch.joelniklaus.indoloc.models.RSSData;
+import ch.joelniklaus.indoloc.models.SensorData;
+import weka.classifiers.trees.RandomForest;
 import weka.core.Instance;
 import weka.core.InstanceComparator;
 import weka.core.Instances;
+import weka.filters.unsupervised.instance.RemovePercentage;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
@@ -17,21 +30,21 @@ import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertTrue;
 
 /**
- * Example local unit test, which will execute on the development machine (host).
+ * Comprises unit tests for all the functionality provided in the WekaHelper class.
  *
- * @see <a href="http://d.android.com/tools/testing">Testing documentation</a>
+ * @author joelniklaus
  */
 public class WekaHelperUnitTest {
 
     private final InstanceComparator comparator = new InstanceComparator();
     private FileHelper fileHelper = new FileHelper();
 
-    protected Instances loadFile(String fileName) throws Exception {
+    protected Instances loadFile(String fileName) throws Exception, CouldNotLoadArffException {
         return fileHelper.loadArff(AbstractTest.ASSETS_PATH + fileName + AbstractTest.ENDING);
     }
 
     @Test
-    public void testMergeInstances() throws Exception, DifferentHeaderException {
+    public void testMergeInstances() throws Exception, DifferentHeaderException, CouldNotLoadArffException {
         Instances train = loadFile("unittests/train_landmark");
         Instances test = loadFile("unittests/test_landmark");
 
@@ -44,15 +57,15 @@ public class WekaHelperUnitTest {
     }
 
     @Test(expected = DifferentHeaderException.class)
-    public void testMergeInstancesException() throws Exception, DifferentHeaderException {
-        Instances train = loadFile("final_cds/train_landmark");
-        Instances test = loadFile("final_cds/test_room");
+    public void testMergeInstancesException() throws Exception, DifferentHeaderException, CouldNotLoadArffException {
+        Instances train = loadFile("unittests/buildInstances");
+        Instances test = loadFile("unittests/duplicates");
 
         Instances data = WekaHelper.mergeInstances(train, test);
     }
 
     @Test
-    public void testRemoveOneAttribute() throws Exception {
+    public void testRemoveOneAttribute() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/duplicates");
 
         // Remove third Attribute (index 2)
@@ -74,7 +87,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testRemoveMultipleAttributes() throws Exception {
+    public void testRemoveMultipleAttributes() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/duplicates");
 
         // Remove third to fifth Attribute (indices 2 to 4)
@@ -94,7 +107,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testRemoveDuplicates() throws Exception {
+    public void testRemoveDuplicates() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/duplicates");
 
         Instances oldData = SerializationUtils.clone(data);
@@ -114,7 +127,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testRoundAttribute() throws Exception {
+    public void testRoundAttribute() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/train_landmark");
 
         // round light to 0.1
@@ -126,7 +139,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testGetEvery2NdInstance() throws Exception {
+    public void testGetEvery2NdInstance() throws Exception, CouldNotLoadArffException {
         Instances train = loadFile("unittests/train_landmark");
 
         // reduce training set by 50%
@@ -142,7 +155,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testGetEvery3RdInstance() throws Exception {
+    public void testGetEvery3RdInstance() throws Exception, CouldNotLoadArffException {
         Instances train = loadFile("unittests/train_landmark");
 
         // reduce training set by 66%
@@ -158,7 +171,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testGetEvery4ThInstance() throws Exception {
+    public void testGetEvery4ThInstance() throws Exception, CouldNotLoadArffException {
         Instances train = loadFile("unittests/train_landmark");
 
         // reduce training set by 75%
@@ -173,12 +186,35 @@ public class WekaHelperUnitTest {
             assertFalse(instancesContainInstance(newTrain, train.instance(i)));
     }
 
-
-
-
-    /*
     @Test
-    public void testGetTestingSetAndGetTrainingSetStratifiedRemoveFolds() throws Exception {
+    public void testConvertToSingleInstance() throws Exception, InvalidRoomException, CouldNotLoadArffException {
+        ArrayList<DataPoint> dataPoints = getDataPoints();
+        DataPoint dataPoint = dataPoints.get(3);
+
+        Instances expected = WekaHelper.buildInstances(dataPoints);
+        expected.delete(0);
+        expected.delete(0);
+        expected.delete(0);
+        expected.delete(1);
+        Instances actual = loadFile("unittests/buildInstances");
+        actual = WekaHelper.convertToSingleInstance(actual, dataPoint);
+        testInstancesEqual(expected, actual);
+    }
+
+    @Test(expected = InvalidRoomException.class)
+    public void testConvertToSingleInstanceException() throws Exception, InvalidRoomException, CouldNotLoadArffException {
+        ArrayList<DataPoint> dataPoints = getDataPoints();
+        DataPoint dataPoint = dataPoints.get(3);
+        dataPoint.setRoom("hallo");
+
+        Instances actual = loadFile("unittests/buildInstances");
+        actual = WekaHelper.convertToSingleInstance(actual, dataPoint);
+    }
+
+
+/*
+    @Test
+    public void testGetTestingSetAndGetTrainingSetStratifiedRemoveFolds() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/remove");
 
         StratifiedRemoveFolds stratifiedRemoveFolds = WekaHelper.randomizeAndGetStratifiedRemoveFolds(data);
@@ -187,9 +223,10 @@ public class WekaHelperUnitTest {
 
         testTrainingAndTestingSet(data, train, test);
     }
+*/
 
     @Test
-    public void testGetTestingSetAndGetTrainingSetRemovePercentage() throws Exception {
+    public void testGetTestingSetAndGetTrainingSetRemovePercentage() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/remove");
 
         RemovePercentage removePercentage = WekaHelper.randomizeAndGetRemovePercentage(data);
@@ -207,7 +244,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testGetTestingSetAndGetTrainingSetWekaStandard() throws Exception {
+    public void testGetTestingSetAndGetTrainingSetWekaStandard() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/remove");
 
         data.randomize(new java.util.Random());    // randomize instance order before splitting dataset
@@ -218,7 +255,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testBuildInstances() throws Exception {
+    public void testBuildInstances() throws Exception, CouldNotLoadArffException {
         ArrayList<DataPoint> dataPoints = getDataPoints();
 
         Instances expected = WekaHelper.buildInstances(dataPoints);
@@ -228,24 +265,7 @@ public class WekaHelperUnitTest {
 
 
     @Test
-    public void testConvertToSingleInstance() throws Exception {
-        ArrayList<DataPoint> dataPoints = getDataPoints();
-        DataPoint dataPoint = dataPoints.get(3);
-
-        Instances expected = WekaHelper.buildInstances(dataPoints);
-        expected.delete(0);
-        expected.delete(0);
-        expected.delete(0);
-        expected.delete(1);
-        Instances actual = loadFile("unittests/buildInstances");
-        WekaHelper.convertToSingleInstance(actual, dataPoint);
-        testInstancesEqual(expected, actual);
-    }
-
-
-
-    @Test
-    public void testRemoveAllOfSpecificClass() throws Exception {
+    public void testRemoveAllOfSpecificClass() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/duplicates");
 
         Instances oldData = SerializationUtils.clone(data);
@@ -262,7 +282,7 @@ public class WekaHelperUnitTest {
 
         // Every instance of the small set should be in the large set
         for (int i = 0; i < data.numInstances(); i++)
-            assertTrue(dataContainsInstance(oldData, data.instance(i)));
+            assertTrue(instancesContainInstance(oldData, data.instance(i)));
 
         // No instance has class value 1
         for (int i = 0; i < data.numInstances(); i++)
@@ -270,7 +290,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testDistributionForInstance() throws Exception {
+    public void testDistributionForInstance() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/remove");
 
         RemovePercentage removePercentage = WekaHelper.randomizeAndGetRemovePercentage(data);
@@ -286,7 +306,7 @@ public class WekaHelperUnitTest {
     }
 
     @Test
-    public void testClassificationDifference() throws Exception {
+    public void testClassificationDifference() throws Exception, CouldNotLoadArffException {
         Instances data = loadFile("unittests/remove");
 
         RemovePercentage removePercentage = WekaHelper.randomizeAndGetRemovePercentage(data);
@@ -327,58 +347,64 @@ public class WekaHelperUnitTest {
 
         // Every instance in the testing set has to be in the data set
         for (int i = 0; i < test.numInstances(); i++)
-            assertTrue(dataContainsInstance(data, test.instance(i)));
+            assertTrue(instancesContainInstance(data, test.instance(i)));
 
         // Every instance in the training set has to be in the data set
         for (int i = 0; i < train.numInstances(); i++)
-            assertTrue(dataContainsInstance(data, train.instance(i)));
+            assertTrue(instancesContainInstance(data, train.instance(i)));
 
         // No instance which is in the training set is in the testing set
         for (int i = 0; i < train.numInstances(); i++)
-            assertFalse(dataContainsInstance(test, train.instance(i)));
+            assertFalse(instancesContainInstance(test, train.instance(i)));
 
         // No instance which is in the testing set is in the training set
         for (int i = 0; i < test.numInstances(); i++)
-            assertFalse(dataContainsInstance(train, test.instance(i)));
+            assertFalse(instancesContainInstance(train, test.instance(i)));
     }
 
-    private void addDataPoint(ArrayList<DataPoint> dataPoints, String room, SensorData sensorData, Integer[] rss) {
-        DataPoint dataPoint = getDataPoint(room, sensorData, rss);
-        dataPoints.add(dataPoint);
-    }
-
-    @NonNull
-    private DataPoint getDataPoint(String room, SensorData sensorData, Integer[] rss) {
-        ArrayList<Integer> rssList = new ArrayList<>();
-        for (Integer rssValue : rss)
-            rssList.add(rssValue);
-        return new DataPoint(room, sensorData, RSSData.createRSSDataTest(rssList));
-    }
-
-
-    private boolean dataContainsInstance(Instances data, Instance instance) {
-        for (int i = 0; i < data.numInstances(); i++)
-            if (comparator.compare(instance, data.instance(i)) == 0)
-                return true;
-        return false;
-    }
 
     @NonNull
     private ArrayList<DataPoint> getDataPoints() {
         ArrayList<DataPoint> dataPoints = new ArrayList<>();
-        Integer[] rss1 = {0, 23, 39, 39, 39, 39, 39, 39};
-        addDataPoint(dataPoints, "stube", new SensorData(17, -39), rss1);
-        Integer[] rss2 = {0, 10, 31, 31, 31, 31, 31, 31};
-        addDataPoint(dataPoints, "kueche", new SensorData(8, -38), rss2);
-        Integer[] rss3 = {0, 16, 16, 23, 23, 23, 14, 14};
-        addDataPoint(dataPoints, "badgross", new SensorData(4, -42), rss3);
-        Integer[] rss4 = {0, 20, 33, 33, 33, 33, 33, 33};
-        addDataPoint(dataPoints, "badklein", new SensorData(5, -44), rss4);
-        Integer[] rss5 = {0, 11, 34, 34, 34, 34, 34, 34};
-        addDataPoint(dataPoints, "gang", new SensorData(11, -39), rss5);
+
+        RSSData rssData1 = new RSSData(new ArrayList<>(Arrays.asList(0, 23, 39, 39, 39, 39, 39, 39, 0, 0)));
+        float[] gravity1 = {8.1111f, 11.1111f, 8.111f};
+        float[] magnetic1 = {3.41111f, 11.4111f, 6.16341f};
+        SensorData sensorData1 = SensorData.getSensorDataTest(0, 121, 12, 0, gravity1, magnetic1, 23.23f, 32.122f, 56.677f, 5.96f);
+        LocationData locationData1 = new LocationData(7.12094, 8.38923);
+        dataPoints.add(new DataPoint("stube", sensorData1, rssData1, locationData1));
+
+        RSSData rssData2 = new RSSData(new ArrayList<>(Arrays.asList(0, 23, 39, 39, 39, 39, 39, 39, 0, 0)));
+        float[] gravity2 = {8.2222f, 22.2222f, 8.222f};
+        float[] magnetic2 = {3.42222f, 22.4222f, 6.26342f};
+        SensorData sensorData2 = SensorData.getSensorDataTest(0, 222, 22, 0, gravity2, magnetic2, 23.23f, 32.222f, 56.677f, 5.96f);
+        LocationData locationData2 = new LocationData(7.22094, 8.38923);
+        dataPoints.add(new DataPoint("kueche", sensorData2, rssData2, locationData2));
+
+        RSSData rssData3 = new RSSData(new ArrayList<>(Arrays.asList(0, 23, 39, 39, 39, 39, 39, 39, 0, 0)));
+        float[] gravity3 = {8.3232f, 32.3222f, 8.233f};
+        float[] magnetic3 = {3.43232f, 22.4222f, 6.26343f};
+        SensorData sensorData3 = SensorData.getSensorDataTest(0, 323, 32, 0, gravity3, magnetic3, 23.23f, 32.322f, 56.677f, 5.96f);
+        LocationData locationData3 = new LocationData(7.32094, 8.38923);
+        dataPoints.add(new DataPoint("badgross", sensorData3, rssData3, locationData3));
+
+        RSSData rssData4 = new RSSData(new ArrayList<>(Arrays.asList(0, 23, 39, 39, 39, 39, 39, 39, 0, 0)));
+        float[] gravity4 = {8.4242f, 42.4222f, 8.244f};
+        float[] magnetic4 = {3.44242f, 22.4222f, 6.26344f};
+        SensorData sensorData4 = SensorData.getSensorDataTest(0, 424, 42, 0, gravity4, magnetic4, 23.23f, 32.422f, 56.677f, 5.96f);
+        LocationData locationData4 = new LocationData(7.42094, 8.38923);
+        dataPoints.add(new DataPoint("badklein", sensorData4, rssData4, locationData4));
+
+        RSSData rssData5 = new RSSData(new ArrayList<>(Arrays.asList(0, 23, 39, 39, 39, 39, 39, 39, 0, 0)));
+        float[] gravity5 = {8.5252f, 52.5222f, 8.255f};
+        float[] magnetic5 = {3.45252f, 22.4222f, 6.26345f};
+        SensorData sensorData5 = SensorData.getSensorDataTest(0, 525, 52, 0, gravity5, magnetic5, 23.23f, 32.522f, 56.677f, 5.96f);
+        LocationData locationData5 = new LocationData(7.52094, 8.38923);
+        dataPoints.add(new DataPoint("gang", sensorData5, rssData5, locationData5));
+
         return dataPoints;
     }
-    */
+
 
     private boolean instancesContainInstance(Instances haystick, Instance needle) {
         for (Instance instance : haystick)
